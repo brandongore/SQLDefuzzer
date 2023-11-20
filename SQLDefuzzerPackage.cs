506bb65d1d92860f90de0b8995c7def7b5f1674e
brandongore <brandongore123@gmail.com>
2023-11-21 00:28:52 +0200

    Setup basic working implementation

--- SQLDefuzzerPackage.cs
+++ SQLDefuzzerPackage.cs
@@ -1,31 +1,16 @@
 ﻿using Microsoft.VisualStudio.Shell;
 using System;
 using System.Runtime.InteropServices;
-using System.Threading;
-using Task = System.Threading.Tasks.Task;
 
 namespace SQLDefuzzer
 {
     /// <summary>
     /// This is the class that implements the package exposed by this assembly.
     /// </summary>
-    /// <remarks>
-    /// <para>
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the
-    /// IVsPackage interface and uses the registration attributes defined in the framework to
-    /// register itself and its components with the shell. These attributes tell the pkgdef creation
-    /// utility what data to put into .pkgdef file.
-    /// </para>
-    /// <para>
-    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
-    /// </para>
-    /// </remarks>
-    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true)]
     [Guid(SQLDefuzzerPackage.PackageGuidString)]
-    public sealed class SQLDefuzzerPackage : AsyncPackage
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    public sealed class SQLDefuzzerPackage : Package
     {
         /// <summary>
         /// SQLDefuzzerPackage GUID string.
@@ -41,11 +26,13 @@ namespace SQLDefuzzer
         /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
         /// <param name="progress">A provider for progress updates.</param>
         /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
-        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        protected override void Initialize()
         {
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
-            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            //await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            DeFuzz.Initialize(this);
+            base.Initialize();
         }
 
         #endregion
